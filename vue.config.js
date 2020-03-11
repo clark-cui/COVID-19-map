768905c22a372715be2a1827edf3360504a2d311
clark-cui <cuirongchuan@163.com>
2020-03-11 22:57:48 +0800

    还原css module

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
     // css预设器配置项
     loaderOptions: {},    
     // 是否启用 CSS modules for all css / pre-processor files.
-    requireModuleExtension: false
+    modules: false
   },  
   // 是否使用 thread-loader
   parallel: require('os').cpus().length > 1, 
