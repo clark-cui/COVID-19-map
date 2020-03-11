4ba0d67170138ac43d6e6a39e8d0f2d8b52ecd79
clark-cui <cuirongchuan@163.com>
2020-03-11 21:57:11 +0800

    修改province引入

--- src/map/province.js
+++ src/map/province.js
@@ -1,5 +1,7 @@
 //批量导入node module里的所有省份地图
-const modulesFiles = require.context('../../node_modules/_echarts@4.6.0@echarts/map/js/province/', true, /.js$/)
+// const modulesFiles = require.context('../../node_modules/_echarts@4.6.0@echarts/map/js/province/', true, /.js$/)
+const modulesFiles = require.context('echarts/map/js/province/', true, /.js$/)
+
 const modules = modulesFiles.keys().reduce(
     (modules, modulePath) => 
     {
